2b7794c6892c79a780001bd90240c7e334b9476d
Faris Mustafa <farismmk@gmail.com>
2017-08-19 19:56:38 -0700

    test lint-staged

--- test/find.js
+++ test/find.js
@@ -1,7 +1,7 @@
 const expect = require('chai').expect;
 
 describe('elmFindDependencies', () => {
-  it('returns vinyl files for all dependencies', () => {
+  it("returns vinyl files for all dependencies", () => {
 
   });
-})
+});
